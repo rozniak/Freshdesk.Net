b8e6c14d1b052cadc820232207e3e4dad6eb8186
Rory Fewell <roryfewell@hotmail.com>
2020-05-11 14:18:59 +0100

    Enhancement: Add FreshdeskObjectKind enumeration

---
+++ Freshdesk/Schema/FreshdeskObjectKind.cs
@@ -0,0 +1,28 @@
+﻿/*
+ * Freshdesk.Schema.FreshdeskObjectKind -- Freshdesk Object Kind Enumeration.
+ *
+ * This source-code is part of the Freshdesk API for C# library by Rory Fewell (rozniak) of Oddmatics for Agile ICT for Education Ltd.:
+ * <<https://oddmatics.uk>>
+ * <<http://www.agileict.co.uk>>
+ * 	
+ * Copyright (C) 2017 Oddmatics
+ * 	
+ * Sharing, editing and general licence term information can be found inside of the "LICENSE.MD" file that should be located in the root of this project's directory structure.
+ */
+
+namespace Freshdesk.Schema
+{
+    /// <summary>
+    /// Defines available Freshdesk object types.
+    /// </summary>
+    public enum FreshdeskObjectKind
+    {
+        Agent,
+        Company,
+        Contact,
+        Conversation,
+        Solution,
+        Ticket,
+        TimeEntry
+    }
+}

b2c324a8076d93e05962ae29cd1acac7078e4492
Rory Fewell <roryfewell@hotmail.com>
2020-06-15 08:34:38 +0100

    Cleaning: Run through unnecessary usings in source

--- Freshdesk/Framework/FreshdeskUtility.cs
+++ Freshdesk/Framework/FreshdeskUtility.cs
@@ -1,9 +1,5 @@
 ﻿using Freshdesk.Schema;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Freshdesk.Framework
 {
--- Freshdesk/FreshdeskService.cs
+++ Freshdesk/FreshdeskService.cs
@@ -15,8 +15,6 @@ using Freshdesk.Schema;
 using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Net;
-using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 
 namespace Freshdesk
